41cd04b127f59a95e32ba976f785af7241438be3
Aitore <aitore.master@gmail.com>
2025-01-04 14:53:21 +0500

    Initial commit - Vite + Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-700">Welcome to your styled React app!</p>
+    </div>
+  );
+}
+
+export default App;
