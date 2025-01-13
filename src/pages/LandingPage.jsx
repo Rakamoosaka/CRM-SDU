69151b77b3b44d52aa006b7c0aedb5d26e2307d2
Aitore Nurkali <aitore.master@gmail.com>
2025-01-13 20:25:39 +0500

    landing page fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,14 +7,7 @@ const LandingPage = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#24252D] to-[#1c1e26] flex flex-col items-center justify-center text-white px-4 sm:px-8">
-      {/* Hero Section / Logo */}
       <div className="w-full h-64 bg-cover bg-center bg-no-repeat relative">
-        {/* Background image (optional)
-        <div
-          className="absolute inset-0 bg-no-repeat bg-center bg-cover bg-black bg-opacity-20"
-          style={{ backgroundImage: `url('/hero-image.jpg')` }}
-        ></div>
-        */}
         {/* Centered Logo */}
         <div className="absolute inset-0 flex flex-col items-center justify-center">
           <div className="w-20 sm:w-24 flex items-center justify-center rounded-md mb-4 shadow-md">
@@ -34,14 +27,14 @@ const LandingPage = () => {
         <button
           onClick={() => navigate("/submit")}
           className="bg-[#33ADA9] hover:bg-teal-600 text-white font-medium text-lg sm:text-xl py-2 px-12 sm:px-20 rounded-md
-                     shadow-md hover:shadow-lg transition-all duration-200"
+               shadow-md hover:shadow-lg transition-all duration-200 min-w-64"
         >
           Proceed
         </button>
         <button
           onClick={() => navigate("/login")}
-          className="bg-gray-700 hover:bg-gray-600 text-white font-medium text-lg sm:text-xl py-2 px-8 sm:px-16 rounded-md
-                     border border-gray-600 shadow-md hover:shadow-lg transition-all duration-200"
+          className="bg-gray-700 hover:bg-gray-600 text-white font-medium text-lg sm:text-xl py-2 px-4 sm:px-12 rounded-md
+               border border-gray-600 shadow-md hover:shadow-lg transition-all duration-200 min-w-64"
         >
           Log in as admin
         </button>
