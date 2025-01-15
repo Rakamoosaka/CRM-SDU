fd63e477b60e68474a706af9500c988fdd36ba6f
Aitore Nurkali <aitore.master@gmail.com>
2025-01-15 11:51:29 +0500

    page 404 better

--- src/pages/ErrorNotFound.jsx
+++ src/pages/ErrorNotFound.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ErrorNotFound = () => {
-  return <div>NOT FOUND</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-7xl font-semibold">404 - Page Not Found</h1>
+    </div>
+  );
 };
 
 export default ErrorNotFound;
---
+++ src/utils/toastUtils.js
@@ -0,0 +1,54 @@
+// src/utils/toastUtils.js
+import { toast, Slide, Bounce, Flip } from "react-toastify";
+
+export const successToast = (text) =>
+  toast.success(text, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Slide,
+  });
+
+export const errorToast = (text) =>
+  toast.error(text, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
+
+export const infoToast = (text) =>
+  toast.info(text, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Flip,
+  });
+
+export const warningToast = (text) =>
+  toast.warn(text, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Flip,
+  });
